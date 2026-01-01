5be83f65cb6bc486b1546a160a2b8e8f28531d9b
ajay dev <devadigaajay1729@gmail.com>
2026-01-01 21:49:28 +0530

    intersection of two lists

--- Singly Linked List/singly_linked_list.py
+++ Singly Linked List/singly_linked_list.py
@@ -556,6 +556,23 @@ class LinkedList:
             
         previous.next = list1 if list1 else list2
         return dummy.next
+    
+    def intersection(self, list1, list2):
+        """
+        Finds the intersection of two linked lists
+        """
+        
+        if list1 is None or list2 is None:
+            return None
+        
+        pointer1 = list1
+        pointer2 = list2
+        
+        while pointer1 != pointer2:
+            pointer1 = pointer1.next if pointer1 else list2
+            pointer2 = pointer2.next if pointer2 else list1
+            
+        return pointer1
         
 linked_list = LinkedList()
 linked_list.append(1)
