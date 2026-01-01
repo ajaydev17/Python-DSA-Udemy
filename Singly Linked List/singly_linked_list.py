63f3ee78a3e7f373793acdb99ccd19463a2a723e
ajay dev <devadigaajay1729@gmail.com>
2026-01-01 21:28:31 +0530

    merge two linked list

--- Singly Linked List/singly_linked_list.py
+++ Singly Linked List/singly_linked_list.py
@@ -535,6 +535,27 @@ class LinkedList:
             
         self.head = dummy.next
         
+    def merge_two_lists(self, list1, list2):
+        
+        """
+        Merges two sorted linked lists into a single sorted linked list
+        """
+        
+        dummy = Node(0)
+        previous = dummy
+        
+        while list1 and list2:
+            if list1.value <= list2.value:
+                previous.next = list1
+                list1 = list1.next
+            else:
+                previous.next = list2
+                list2 = list2.next
+                
+            previous = previous.next
+            
+        previous.next = list1 if list1 else list2
+        return dummy.next
         
 linked_list = LinkedList()
 linked_list.append(1)
