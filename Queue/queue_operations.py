8063a7129aaf5014701873fdca5b31f1a16ced1f
ajay dev <devadigaajay1729@gmail.com>
2026-01-10 01:14:23 +0530

    queue dequeue operation

--- Queue/queue_operations.py
+++ Queue/queue_operations.py
@@ -39,3 +39,20 @@ class Queue:
             self.last = new_node
         self.length += 1
         return True
+
+    def dequeue(self):
+        """
+        Dequeues the first node from the queue
+        """
+        
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+        temp.next = None
+        self.length -= 1
+        return temp
