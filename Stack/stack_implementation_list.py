18ba8557c094a7cec7db04f7939c5aba030a31ec
ajay dev <devadigaajay1729@gmail.com>
2026-01-12 21:51:53 +0530

    stack implementation using list push operation

--- Stack/stack_implementation_list.py
+++ Stack/stack_implementation_list.py
@@ -9,5 +9,13 @@ class Stack:
         """
         
         # iterate through the stack
-        for i in range(len(self.stack)):
+        for i in range(len(self.stack) - 1, -1, -1):
             print(self.stack[i], end="->")
+
+    def push(self, value):
+        """
+        Pushes a new node with the given value to the top of the stack
+        """
+        
+        self.stack.append(value)
+        return True
